a406ad0ff74506f31c57e74f9d44bbe47ac4653d
jennyelit <jenniffertrelles@gmail.com>
2016-02-15 22:59:40 -0500

    ds
    xcxc

--- ProyectoParcial2/ProyectoParcial2/MainWindow.xaml.vb
+++ ProyectoParcial2/ProyectoParcial2/MainWindow.xaml.vb
@@ -5,6 +5,24 @@ Class MainWindow
     Public loggedIn As Boolean
     Public cuentaIngresada As Cuentas
 
+    Private Sub Window_Loaded(sender As Object, e As RoutedEventArgs)
+        Dim dbath = "C:\bdProyecto.mdb"
+        Dim strConexion = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" & dbath
+
+        Using conexion As New OleDbConnection(strConexion)
+            Dim query As String = "Select * From Usuarios;"
+            Dim adapter As New OleDbDataAdapter(query, conexion)
+            Dim dsCuentas As New DataSet("cuenta")
+            adapter.Fill(dsCuentas, "cuenta")
+            Me.cuentas = New ArrayList
+
+            For Each u As DataRow In dsCuentas.Tables("cuenta").Rows
+                Me.cuentas.Add(New Cuentas(u))
+            Next
+        End Using
+    End Sub
+
+
     Private Sub lblRegistro_MouseDown(sender As Object, e As MouseButtonEventArgs) Handles lblRegistro.MouseDown
         MessageBox.Show("Shalalalaaa")
     End Sub
@@ -24,25 +42,12 @@ Class MainWindow
             txtPass.Password = ""
             txtUser.Text = ""
         Else
-                MessageBox.Show("Credenciales incorrectas")
-            End If
+            MessageBox.Show("Credenciales incorrectas")
+            txtPass.Password = ""
+            txtUser.Text = ""
+        End If
 
     End Sub
 
-    Private Sub Window_Loaded(sender As Object, e As RoutedEventArgs)
-        Dim dbath = "C:\bdProyecto.mdb"
-        Dim strConexion = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" & dbath
 
-        Using conexion As New OleDbConnection(strConexion)
-            Dim query As String = "Select * From Usuarios;"
-            Dim adapter As New OleDbDataAdapter(query, conexion)
-            Dim dsCuentas As New DataSet("Datos")
-            adapter.Fill(dsCuentas, "cuenta")
-            Me.cuentas = New ArrayList
-
-            For Each u As DataRow In dsCuentas.Tables("cuenta").Rows
-                Me.cuentas.Add(New Cuentas(u))
-            Next
-        End Using
-    End Sub
 End Class
--- ProyectoParcial2/ProyectoParcial2/WinUsuario.xaml.vb
+++ ProyectoParcial2/ProyectoParcial2/WinUsuario.xaml.vb
@@ -1,9 +1,29 @@
-﻿Public Class WinUsuario
+﻿Imports System.Data.OleDb
+Imports System.Data
+Public Class WinUsuario
     Dim winLoggin As MainWindow
+    Public juego As Juego
+    Property privilegios As String
+    Dim dsjuegos As New DataSet("juegos")
+    Dim dbath = "C:\bdProyecto.mdb"
+    Dim strConexion = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" & dbath
 
     Private Sub Window_Loaded(sender As Object, e As RoutedEventArgs)
-        winLoggin = Me.Owner
-        Userlbl.Content = "Bienvenid@ " + winLoggin.cuentaIngresada.Nombre
+
+
+        Using conexion As New OleDbConnection(strConexion)
+            lblUser.Content = "Bienvenid@ " + winLoggin.cuentaIngresada.Nombre
+            Dim query As String = "Select * From Juegos;" 'comando para consulta en base que se ha llamado
+            Dim adapter As New OleDbDataAdapter(query, conexion) 'con la conexion tratar de acceder a los datos
+            Dim builder As New OleDbCommandBuilder(adapter)
+            adapter.Fill(dsjuegos, "juegos")
+            adapter.MissingSchemaAction = MissingSchemaAction.AddWithKey
+
+            Me.dgDescripcionVideojuegos.ItemsSource = dsjuegos.Tables("juegos").DefaultView
+            'dgDescripcionVideojuegos.DataContext = dsjuegos
+            winLoggin = Me.Owner
+
+        End Using
     End Sub
 
     Private Sub Label_MouseDown(sender As Object, e As MouseButtonEventArgs)
@@ -18,7 +38,7 @@
         Me.Close()
     End Sub
 
-   
+
 
     Private Sub menuEditar_Click(sender As Object, e As RoutedEventArgs)
         Dim winModificar As New Modificar
@@ -27,7 +47,27 @@
         Me.Hide()
     End Sub
 
+    'Private Sub Sub_Eliminar_Click(sender As Object, e As RoutedEventArgs) Handles Sub_Eliminar.Click
+    '    MessageBox.Show("Esta seguro que desea eliminar el juego" + juego.Titulo, "Juego a Eliminar", MessageBoxButton.OKCancel, MessageBoxImage.Stop)
+    'End Sub
+
+
+    Private Sub btnComprar_Click(sender As Object, e As RoutedEventArgs) Handles btnComprar.Click
+        Dim contador As Integer = 0
+        txtxNumCompra.Text = contador + 1 + "juegos seleccionados"
+    End Sub
+
+
     Private Sub menuCrear_Click(sender As Object, e As RoutedEventArgs)
+        'Dim ventanaAgregar As New Agregar
+        'ventanaAgregar.Owner = Me
+
+        'ventanaAgregar.Show()
+        ''Me.Hide()
+        'Me.IsEnabled = False
+    End Sub
+
+    Private Sub MenuItem_Click(sender As Object, e As RoutedEventArgs)
 
     End Sub
 End Class
